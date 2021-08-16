0d9a31fe7a89496be5b737f6ae92d3b189defe8f
satoo_hiro <h.s.nagano.t@gmail.com>
2021-08-16 22:20:30 +0900

    batsu修正

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -1,9 +1,11 @@
 $(".l-header__left__menu__botton").click(function () {//ボタンがクリックされたら
-  $(".l-sidebar").toggleClass("__is-open");//ボタン自身に activeクラスを付与し
+  $(".l-sidebar").toggleClass("__is-open");
+  $(".l-sidebar__batsu").toggleClass("__is-open")//ボタン自身に activeクラスを付与し
 });
 
 $(".l-sidebar__batsu").click(function () {//ボタンがクリックされたら
-  $(".l-sidebar").toggleClass("__is-open");//ボタン自身に activeクラスを付与し
+  $(".l-sidebar").toggleClass("__is-open");
+  $(".l-sidebar__batsu").toggleClass("__is-open")//ボタン自身に activeクラスを付与し
 });
 
 

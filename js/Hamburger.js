829c16d85464c16d311ca9001363c3e9ffcf52a6
satoo_hiro <h.s.nagano.t@gmail.com>
2021-09-05 18:55:07 +0900

    サイドバー　スクロール対応

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -1,9 +1,9 @@
-$(".l-header__left__menu__botton").click(function () {//ボタンがクリックされたら
+$(".l-header__left__menu__botton").click(function () {
   $(".l-sidebar").toggleClass("__is-open");
-  $(".l-sidebar__batsu").toggleClass("__is-open")//ボタン自身に activeクラスを付与し
+  $(".l-sidebar__batsu").toggleClass("__is-open")
 });
 
-$(".l-sidebar__batsu").click(function () {//ボタンがクリックされたら
+$(".l-sidebar__batsu").click(function () {
   $(".l-sidebar").toggleClass("__is-open");
-  $(".l-sidebar__batsu").toggleClass("__is-open")//ボタン自身に activeクラスを付与し
+  $(".l-sidebar__batsu").toggleClass("__is-open")
 });
